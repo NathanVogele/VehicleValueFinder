73a8da67f0b29e16d1d11979617e3944b6b8c87a
NathanVogele <63888079+NathanVogele@users.noreply.github.com>
2021-06-28 16:54:59 -0500

    Added Bitly Integration.

--- shorten_url.py
+++ shorten_url.py
@@ -6,31 +6,67 @@ import api_keys
 import requests
 import logging
 
-# Some Basic Setup
 tiny_url_key = api_keys.tinyurl_key
 logging.basicConfig(level=logging.DEBUG)
 
 
-def shorten_url(url, campaign, tags):
-    longURL = url
+# Tags has to be an array of tags.
+# Group can be anything, but is a custom domain to t
+def shorten_url_bitly(url, tags, campaign):
 
-    # Tags can be comma sperated
-    urlTags = tags
-    urlCampaign = campaign
     headers = {
-        'accept': 'application/json',
-        'Authorization': 'Bearer {' + tiny_url_key + '}',
+        'Authorization': 'Bearer ' + api_keys.BIT_API_KEY,
         'Content-Type': 'application/json',
     }
 
-    # Formats the Post Request
-    data = '{ "url": "' + longURL + '", "domain": "tiny.one", "alias": "' + str(urlCampaign) + '", "tags":  ' + urlTags + ' }'
+    # Formats the Post Request -- BELOW IS THE DEFAULT PROVIDED BY BitLy
+    # data = '{ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" }'
 
-    response = requests.post('https://api.tinyurl.com/create', headers=headers, data=data)
-    logging.debug(response)
+    data = '{ "long_url": "' + url + '", "domain": "bit.ly", "group_guid": "Bk7ofsao0js" }'
+
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+    res = response.json()
+
+    logging.debug(res)
 
     # Check to make sure the response is a string
-    shortURL = str(response.tiny_url)
-    logging.debug(shortURL)
 
-    return shortURL
+    bitly_link = (res["link"])
+    logging.debug(bitly_link)
+
+    return bitly_link
+
+
+# def shorten_url_tiny(url, campaign, tags):
+#     longURL = url
+#
+#     # Tags can be comma sperated
+#     urlTags = tags
+#     urlCampaign = campaign
+#     headers = {
+#         'accept': 'application/json',
+#         'Authorization': 'Bearer {' + tiny_url_key + '}',
+#         'Content-Type': 'application/json',
+#     }
+#
+#     # Formats the Post Request
+#     data = '{ "url": "' + longURL + '", "domain": "tiny.one", "alias": "' + str(
+#         urlCampaign) + '", "tags":  ' + urlTags + ' }'
+#
+#     response = requests.post('https://api.tinyurl.com/create', headers=headers, data=data)
+#     logging.debug(response)
+#
+#     # Check to make sure the response is a string
+#     shortURL = str(response.tiny_url)
+#     logging.debug(shortURL)
+#
+#     return shortURL
+
+
+campaign = "Test_Mailer"
+
+tags = "Martin_City"
+
+shortURL = shorten_url_bitly("https://www.MartinCityMarketing.com/thisisatest", tags, campaign)
+
+print("The Short URL IS " + shortURL)
